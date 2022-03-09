9fda848c8f27eecad7f29f678d24fdb5e598e0a7
Kevin Jan <kevin.jan.tw@gmail.com>
2022-03-09 21:15:26 +0800

    Delete configuration directory

--- configuration/Network_setting/settingNetwork_sf1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ip netns add sf1
-ip link set dev enp3s0f1 netns sf1
-ip netns exec sf1 ip addr add 192.168.50.207/24 dev enp3s0f1
-ip netns exec sf1 ip link set dev enp3s0f1 up
-ip netns exec sf1 ifconfig
