3e317352df3256bd881845dd73b68dbb2d583d78
Kevin Jan <kevin.jan.tw@gmail.com>
2022-03-11 20:23:57 +0800

    Delete configuration directory

--- configuration/network_setting/settingNetwork_sf0.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ip netns add sf0
-ip link set dev enp3s0f0 netns sf0
-ip netns exec sf0 ip addr add 192.168.20.100/24 dev enp3s0f0
-ip netns exec sf0 ip link set dev enp3s0f0 up
-ip netns exec sf0 ifconfig
